5a557855674ed7b51562e7d13319cc38abb94296
junseok Kim <junseokkim00@berkeley.edu>
2022-10-28 21:09:13 -0700

    crawling_ver2 completed

--- tether/crawl_ver2.py
+++ tether/crawl_ver2.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 url = "https://guide.berkeley.edu/courses/"
-file = open("data2.html","w")
+file = open("data2.html", "w")
 response = requests.get(url)
 file.write(response.text)
 file.close()
-soup = BeautifulSoup(response.text,'html.parser')
-soupPart = soup.find("div",id="atozindex")
+soup = BeautifulSoup(response.text, 'html.parser')
+soupPart = soup.find("div", id="atozindex")
 soupCategories = soupPart.find_all("li")
-Categories = open("result2.txt","w")
+Categories = open("result2.txt", "w")
 for category in soupCategories:
     Categories.write(category.get_text()+'\n')
 Categories.close()
